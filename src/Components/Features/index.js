248c7179743b15a4cc2112c2af9f184f8f03f48b
Dhruvil padshala <padshaladhruvil5@gmail.com>
2024-08-05 00:50:02 +0530

    description and other changes

--- src/Components/Features/index.js
+++ src/Components/Features/index.js
@@ -11,9 +11,9 @@ import { styled } from "@mui/material/styles";
 import BusinessIcon from "@mui/icons-material/Business";
 import PhoneAndroidIcon from "@mui/icons-material/PhoneAndroid";
 import TvIcon from "@mui/icons-material/Tv";
-import ChatIcon from "@mui/icons-material/Chat";
 import SignalCellularAltIcon from "@mui/icons-material/SignalCellularAlt";
-import AppsIcon from "@mui/icons-material/Apps";
+import CallToActionIcon from "@mui/icons-material/CallToAction";
+import WysiwygIcon from "@mui/icons-material/Wysiwyg";
 
 const Root = styled(Box)(({ theme }) => ({
   backgroundColor: "#f0f2f5",
@@ -35,8 +35,9 @@ const SectionTitle = styled(Typography)(({ theme }) => ({
 
 const CustomCard = styled(Card)(({ theme, title }) => ({
   display: "flex",
-  width: "350px",
+  width: "530px",
   height: "250px",
+  backgroundColor: "#f0f2f5",
   flexDirection: "column",
   justifyContent: "center",
   alignItems: "flex-start",
@@ -46,7 +47,7 @@ const CustomCard = styled(Card)(({ theme, title }) => ({
   "&:hover": {
     transform: "translateY(-5px)",
     boxShadow: "0 8px 12px rgba(0, 0, 0, 0.2)",
-    backgroundColor: "blue", 
+    backgroundColor: "blue",
     "& .MuiCardContent-root, & .MuiTypography-root, & .MuiSvgIcon-root": {
       color: "white",
     },
@@ -63,7 +64,7 @@ const CustomCard = styled(Card)(({ theme, title }) => ({
 
 const IconWrapper = styled("div")(({ theme }) => ({
   marginBottom: theme.spacing(1),
-  color: "#007bff", // Blue color for icons
+  color: "#007bff",
   fontSize: "3rem",
   [theme.breakpoints.down("sm")]: {
     fontSize: "2rem",
@@ -71,59 +72,66 @@ const IconWrapper = styled("div")(({ theme }) => ({
 }));
 
 const CardContent = styled(MuiCardContent)(({ theme }) => ({
-  transition: "background-color 0.3s ease, transform 0.3s ease, color 0.3s ease",
+  transition:
+    "background-color 0.3s ease, transform 0.3s ease, color 0.3s ease",
 }));
 
 const services = [
   {
-    icon: <BusinessIcon fontSize="large" />,
-    title: "Business Strategy",
+    icon: <CallToActionIcon fontSize="large" />,
+    title: "UI/UX Design",
     description:
-      "I throw myself down among the tall grass by the stream as I lie close to the earth.",
+      "We craft user-centric designs that elevate your brand and enhance user experience. Partner with us to create intuitive, visually stunning interfaces tailored to your needs.",
+  },
+  {
+    icon: <BusinessIcon fontSize="large" />,
+    title: "IT Consulting",
+    description:
+      "Our team delivers tailored technology strategies and support to drive innovation and efficiency. Let us help you navigate complex IT challenges and achieve your goals seamlessly.",
+  },
+  {
+    icon: <TvIcon fontSize="large" />,
+    title: "Web Development",
+    description:"Crafting engaging and functional websites with a focus on clean design and responsive functionality. Explore my portfolio to see how I turn innovative ideas into compelling digital experiences.",
   },
   {
     icon: <PhoneAndroidIcon fontSize="large" />,
     title: "App Development",
-    description:
-      "It uses a dictionary of over 200 Latin words, combined with a handful of model sentence.",
-  },
+    description:"We specialize in crafting custom mobile and web applications tailored to your business needs. Our expert team combines cutting-edge technology with user-centric design to deliver seamless, high-performance solutions.",  },
   {
-    icon: <TvIcon fontSize="large" />,
-    title: "App Development",
-    description:
-      "I throw myself down among the tall grass by the stream as I lie close to the earth.",
-  },
-  {
-    icon: <ChatIcon fontSize="large" />,
-    title: "Mobile App",
-    description:
-      "I throw myself down among the tall grass by the stream as I lie close to the earth.",
-  },
+    icon: <WysiwygIcon fontSize="large" />,
+    title: "Software Development",
+    description:"Our SaaS offerings provide tailored software development to meet your unique needs, from initial design to ongoing support. Enhance efficiency and drive innovation with our expert team.",  },
+
   {
     icon: <SignalCellularAltIcon fontSize="large" />,
-    title: "CEO Marketing",
-    description:
-      "It uses a dictionary of over 200 Latin words, combined with a handful of model sentence.",
-  },
-  {
-    icon: <AppsIcon fontSize="large" />,
-    title: "Personal Portfolio April",
-    description:
-      "I throw myself down among the tall grass by the stream as I lie close to the earth.",
-  },
+    title: "Social Media Marketing",
+    description:"Boost your online presence with our tailored social media strategies. From engaging content to targeted ads, we drive growth and enhance your brand’s reach. Let’s connect and transform your social media into a powerful marketing tool.",  },
 ];
 
 const Features = () => {
   return (
     <Root>
-      <Container sx={{ mt: { xs: 40, md: -10 }, justifyContent: { xs: "center", md: "flex-start" } }}>
-        <Typography variant="h6" color="primary">Features</Typography>
-        <SectionTitle variant="h4" gutterBottom mt={{ xs: 2, md: 5 }} sx={{ textAlign: { xs: "left", md: "left" } }}>
+      <Container
+        sx={{
+          mt: { xs: 40, md: 0 },
+          justifyContent: { xs: "center", md: "flex-start" },
+        }}
+      >
+        <Typography variant="h6" color="primary">
+          Features
+        </Typography>
+        <SectionTitle
+          variant="h4"
+          gutterBottom
+          mt={{ xs: 2, md: 5 }}
+          sx={{ textAlign: { xs: "left", md: "left" } }}
+        >
           What I Do
         </SectionTitle>
         <Grid container spacing={4} sx={{ mt: { xs: 2, md: 5 } }}>
           {services.map((service, index) => (
-            <Grid item xs={12} sm={6} md={4} key={index}>
+            <Grid item xs={12} sm={6} key={index}>
               <CustomCard title={service.title}>
                 <IconWrapper>{service.icon}</IconWrapper>
                 <CardContent>
