af7b0281cbe438275b64be2e06b7ed13ed167812
Dhruvil padshala <padshaladhruvil5@gmail.com>
2024-08-10 17:00:16 +0530

    Contact are complate

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./Pages/Header";
 import Home from "./Pages/Home"
 import Features from "./Pages/Features"
 import Portfolio from "./Pages/Portfolio"
+import Contact from "./Pages/Contact"
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Home />
       <Features />
       <Portfolio />
+      <Contact />
     </div>
   );
 }
