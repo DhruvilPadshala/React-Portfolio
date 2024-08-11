72251ef44a7f48c0eff4fd782e842fe06728122d
Dhruvil padshala <padshaladhruvil5@gmail.com>
2024-08-11 15:00:55 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import "./App.css";
+// import "./App.css";
 import Header from "./Pages/Header";
-import Home from "./Pages/Home";
-import Features from "./Pages/Features";
-import Portfolio from "./Pages/Portfolio";
-import Resume from "./Pages/Resume";
-import Contact from "./Pages/Contact";
+import Home from "./Pages/Home"
+import Features from "./Pages/Features"
+import Portfolio from "./Pages/Portfolio"
+<<<<<<< Resume
+import Resume from './Pages/Resume'
+=======
+import Contact from "./Pages/Contact"
+>>>>>>> main
 
 function App() {
   return (
@@ -13,8 +16,11 @@ function App() {
       <Home />
       <Features />
       <Portfolio />
-      <Resume />
+<<<<<<< Resume
+      <Resume/>
+=======
       <Contact />
+>>>>>>> main
     </div>
   );
 }
